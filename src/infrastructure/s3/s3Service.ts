a556a2ecd7faf48b3f05a609027de5e0a8a24f48
Robert Waring <Robert.Waring@bjss.com>
2021-03-11 15:40:30 +0000

    chore(cvsb-19156): Added logging for incorrect content type

--- src/infrastructure/s3/s3Service.ts
+++ src/infrastructure/s3/s3Service.ts
@@ -16,6 +16,7 @@ export default async (s3: S3, bucket: string, folder: string, certId: string, vi
     .promise();
 
   if (response.ContentType !== 'application/pdf') {
+    console.error(`Incorrect content-type: ${response.ContentType}`);
     throw new IncorrectFileTypeError();
   }
 

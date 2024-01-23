2b5d7aa6c652a760782ab1134d876c32431f8377
Shivangi Das <shivangi.das7@gmail.com>
2024-01-23 12:10:13 +0000

    fix(cb2-10457): supress lint errors

--- src/domain/getFile.ts
+++ src/domain/getFile.ts
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-base-to-string */
+
 import { S3, AWSError } from 'aws-sdk';
 import { APIGatewayProxyResult } from 'aws-lambda';
 import getObjectFromS3 from '../infrastructure/s3/s3PlateService';
